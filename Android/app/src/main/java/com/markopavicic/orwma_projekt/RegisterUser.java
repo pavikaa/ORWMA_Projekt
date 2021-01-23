5d682af902f49cc9fa06a832bb64b5e3581aaf39
Marko Pavičić <mpavicic99@gmail.com>
2021-01-23 16:24:34 +0100

    Improve security, stop unnecessary saving of user passwords to database

--- Android/app/src/main/java/com/markopavicic/orwma_projekt/RegisterUser.java
+++ Android/app/src/main/java/com/markopavicic/orwma_projekt/RegisterUser.java
@@ -90,7 +90,7 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            User user = new User(username, password, fullName);
+                            User user = new User(username, fullName);
                             FirebaseDatabase.getInstance().getReference("Players")
                                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
--- Android/app/src/main/java/com/markopavicic/orwma_projekt/User.java
+++ Android/app/src/main/java/com/markopavicic/orwma_projekt/User.java
@@ -3,15 +3,13 @@ package com.markopavicic.orwma_projekt;
 public class User {
     public String fullName;
     public String username;
-    public String password;
 
     public User() {
 
     }
 
-    public User(String username, String password, String fullName) {
+    public User(String username, String fullName) {
         this.username = username;
-        this.password = password;
         this.fullName = fullName;
     }
 
